fd9e1675be9237fa262c0fc6eb1e846206591aff
kenedijantara <kenedijantara@gmail.com>
2026-05-27 20:30:54 -0300

    Leitura inicial do CSV

--- miniprojeto.py
+++ miniprojeto.py
@@ -6,3 +6,18 @@ print("INÍCIO DA ANÁLISE DE DADOS - VAREJO")
 print("=" * 50)
 
 nome_arquivo = 'Base Varejo.csv'
+
+# PASSO 1 - Leitura inicial do arquivo CSV
+print("\n[PASSO 1] Leitura inicial do arquivo CSV:")
+
+with open(nome_arquivo, mode='r', encoding='utf-8') as arquivo:
+
+    leitor = csv.DictReader(arquivo, delimiter=';')
+
+    for indice, linha in enumerate(leitor):
+
+        if indice < 2:
+            print(f"Registro {indice + 1}: {linha}")
+
+        else:
+            break
